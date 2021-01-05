26fbc68abf18cebb2012ddb8499fcff9fd800b73
jeancarlo13 <juancarloschavezortega@gmail.com>
2021-01-05 13:48:15 -0700

    Adds .net 5.0 app for demo

---
+++ Test5.0/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Test5._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
